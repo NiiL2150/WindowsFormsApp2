ae4066db9985e57b4a082fa504fee1cc398d2502
NiiL2150 <nail.shamsudinov@gmail.com>
2021-06-20 19:06:28 +0400

    Update

--- WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -31,19 +31,21 @@ namespace WindowsFormsApp2
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.SaveButton = new System.Windows.Forms.Button();
-            this.LoadButton = new System.Windows.Forms.Button();
+            this.OpenButton = new System.Windows.Forms.Button();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.openToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
             this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
-            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
-            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.symbolsCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.wordsCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.wordsCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.symbolsCountToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonLocalEN = new System.Windows.Forms.Button();
+            this.buttonLocalRU = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -67,15 +69,15 @@ namespace WindowsFormsApp2
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
-            // LoadButton
+            // OpenButton
             // 
-            this.LoadButton.Location = new System.Drawing.Point(367, 235);
-            this.LoadButton.Name = "LoadButton";
-            this.LoadButton.Size = new System.Drawing.Size(75, 23);
-            this.LoadButton.TabIndex = 2;
-            this.LoadButton.Text = "Load";
-            this.LoadButton.UseVisualStyleBackColor = true;
-            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
+            this.OpenButton.Location = new System.Drawing.Point(367, 235);
+            this.OpenButton.Name = "OpenButton";
+            this.OpenButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenButton.TabIndex = 2;
+            this.OpenButton.Text = "Open";
+            this.OpenButton.UseVisualStyleBackColor = true;
+            this.OpenButton.Click += new System.EventHandler(this.OpenButton_Click);
             // 
             // menuStrip1
             // 
@@ -91,8 +93,8 @@ namespace WindowsFormsApp2
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
             this.openToolStripMenuItem,
-            this.openToolStripMenuItem1,
             this.saveToolStripMenuItem,
             this.toolStripSeparator1,
             this.exitToolStripMenuItem});
@@ -100,29 +102,73 @@ namespace WindowsFormsApp2
             this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.fileToolStripMenuItem.Text = "File";
             // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.openToolStripMenuItem.Text = "New";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.openToolStripMenuItem.Text = "Open";
             this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
-            // 
-            // openToolStripMenuItem1
-            // 
-            this.openToolStripMenuItem1.Name = "openToolStripMenuItem1";
-            this.openToolStripMenuItem1.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
-            this.openToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
-            this.openToolStripMenuItem1.Text = "Open";
-            this.openToolStripMenuItem1.Click += new System.EventHandler(this.openToolStripMenuItem1_Click);
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
             this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
             this.saveToolStripMenuItem.Text = "Save";
             this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(143, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // infoToolStripMenuItem
+            // 
+            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.wordsCountToolStripMenuItem,
+            this.symbolsCountToolStripMenuItem});
+            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
+            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.infoToolStripMenuItem.Text = "Info";
+            // 
+            // wordsCountToolStripMenuItem
+            // 
+            this.wordsCountToolStripMenuItem.Checked = true;
+            this.wordsCountToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.wordsCountToolStripMenuItem.Name = "wordsCountToolStripMenuItem";
+            this.wordsCountToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.W)));
+            this.wordsCountToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.wordsCountToolStripMenuItem.Text = "Words count";
+            this.wordsCountToolStripMenuItem.Click += new System.EventHandler(this.wordsCountToolStripMenuItem_Click);
+            // 
+            // symbolsCountToolStripMenuItem
+            // 
+            this.symbolsCountToolStripMenuItem.Checked = true;
+            this.symbolsCountToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.symbolsCountToolStripMenuItem.Name = "symbolsCountToolStripMenuItem";
+            this.symbolsCountToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
+            | System.Windows.Forms.Keys.S)));
+            this.symbolsCountToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
+            this.symbolsCountToolStripMenuItem.Text = "Symbols count";
+            this.symbolsCountToolStripMenuItem.Click += new System.EventHandler(this.symbolsCountToolStripMenuItem_Click);
             // 
             // statusStrip1
             // 
@@ -140,57 +186,35 @@ namespace WindowsFormsApp2
             this.toolStripStatusLabel1.Size = new System.Drawing.Size(119, 17);
             this.toolStripStatusLabel1.Text = "Words: 0; Symbols: 0;";
             // 
-            // infoToolStripMenuItem
+            // buttonLocalEN
             // 
-            this.infoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.wordsCountToolStripMenuItem,
-            this.symbolsCountToolStripMenuItem});
-            this.infoToolStripMenuItem.Name = "infoToolStripMenuItem";
-            this.infoToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
-            this.infoToolStripMenuItem.Text = "Info";
+            this.buttonLocalEN.Location = new System.Drawing.Point(122, 234);
+            this.buttonLocalEN.Name = "buttonLocalEN";
+            this.buttonLocalEN.Size = new System.Drawing.Size(75, 23);
+            this.buttonLocalEN.TabIndex = 5;
+            this.buttonLocalEN.Text = "EN";
+            this.buttonLocalEN.UseVisualStyleBackColor = true;
+            this.buttonLocalEN.Click += new System.EventHandler(this.buttonLocalEN_Click);
             // 
-            // symbolsCountToolStripMenuItem
+            // buttonLocalRU
             // 
-            this.symbolsCountToolStripMenuItem.Checked = true;
-            this.symbolsCountToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.symbolsCountToolStripMenuItem.Name = "symbolsCountToolStripMenuItem";
-            this.symbolsCountToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.S)));
-            this.symbolsCountToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
-            this.symbolsCountToolStripMenuItem.Text = "Symbols count";
-            this.symbolsCountToolStripMenuItem.Click += new System.EventHandler(this.symbolsCountToolStripMenuItem_Click);
-            // 
-            // wordsCountToolStripMenuItem
-            // 
-            this.wordsCountToolStripMenuItem.Checked = true;
-            this.wordsCountToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.wordsCountToolStripMenuItem.Name = "wordsCountToolStripMenuItem";
-            this.wordsCountToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)(((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Shift) 
-            | System.Windows.Forms.Keys.W)));
-            this.wordsCountToolStripMenuItem.Size = new System.Drawing.Size(225, 22);
-            this.wordsCountToolStripMenuItem.Text = "Words count";
-            this.wordsCountToolStripMenuItem.Click += new System.EventHandler(this.wordsCountToolStripMenuItem_Click);
-            // 
-            // toolStripSeparator1
-            // 
-            this.toolStripSeparator1.Name = "toolStripSeparator1";
-            this.toolStripSeparator1.Size = new System.Drawing.Size(177, 6);
-            // 
-            // exitToolStripMenuItem
-            // 
-            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Alt | System.Windows.Forms.Keys.F4)));
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            this.buttonLocalRU.Location = new System.Drawing.Point(255, 234);
+            this.buttonLocalRU.Name = "buttonLocalRU";
+            this.buttonLocalRU.Size = new System.Drawing.Size(75, 23);
+            this.buttonLocalRU.TabIndex = 6;
+            this.buttonLocalRU.Text = "RU";
+            this.buttonLocalRU.UseVisualStyleBackColor = true;
+            this.buttonLocalRU.Click += new System.EventHandler(this.buttonLocalRU_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(454, 289);
+            this.Controls.Add(this.buttonLocalRU);
+            this.Controls.Add(this.buttonLocalEN);
             this.Controls.Add(this.statusStrip1);
-            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.OpenButton);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.menuStrip1);
@@ -211,11 +235,11 @@ namespace WindowsFormsApp2
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button SaveButton;
-        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.Button OpenButton;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
-        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem1;
         private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
         private System.Windows.Forms.StatusStrip statusStrip1;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
@@ -224,6 +248,8 @@ namespace WindowsFormsApp2
         private System.Windows.Forms.ToolStripMenuItem wordsCountToolStripMenuItem;
         private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
         private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button buttonLocalEN;
+        private System.Windows.Forms.Button buttonLocalRU;
     }
 }
 
--- WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/Form1.cs
@@ -5,6 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,9 +15,53 @@ namespace WindowsFormsApp2
 {
     public partial class Form1 : Form
     {
+        string localGlobal = "en";
+
         public Form1()
         {
             InitializeComponent();
+            SetLocalization(localGlobal);
+        }
+
+        string GetLocal(string local)
+        {
+            return loc.ResourceManager.GetString(local);
+        }
+
+        void SetLocalization(string local)
+        {
+            newToolStripMenuItem.Text = GetLocal(local + "_new");
+            saveToolStripMenuItem.Text = GetLocal(local + "_save");
+            openToolStripMenuItem.Text = GetLocal(local + "_open");
+            fileToolStripMenuItem.Text = GetLocal(local + "_file");
+            exitToolStripMenuItem.Text = GetLocal(local + "_exit");
+            infoToolStripMenuItem.Text = GetLocal(local + "_info");
+            wordsCountToolStripMenuItem.Text = GetLocal(local + "_wordsCount");
+            symbolsCountToolStripMenuItem.Text = GetLocal(local + "_symbolsCount");
+            SaveButton.Text = saveToolStripMenuItem.Text = GetLocal(local + "_save");
+            OpenButton.Text = saveToolStripMenuItem.Text = GetLocal(local + "_open");
+
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            int wordCount = textBox1.Text.Split(delimiters, StringSplitOptions.RemoveEmptyEntries).Length
+                , symbolCount = textBox1.Text.Length;
+            if (wordsCountToolStripMenuItem.Checked)
+            {
+                if (symbolsCountToolStripMenuItem.Checked)
+                {
+                    toolStripStatusLabel1.Text = $"{GetLocal(local + "_words")}: {wordCount}; {GetLocal(local + "_symbols")}: {symbolCount}";
+                }
+                else
+                {
+                    toolStripStatusLabel1.Text = $"{GetLocal(local + "_words")}: {wordCount};";
+                }
+            }
+            else
+            {
+                if (symbolsCountToolStripMenuItem.Checked)
+                {
+                    toolStripStatusLabel1.Text = $"{GetLocal(local + "_symbols")}: {symbolCount};";
+                }
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -26,7 +72,7 @@ namespace WindowsFormsApp2
         private void SaveButton_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            saveFileDialog.Filter = $"{GetLocal(localGlobal + "_textFiles")} (*.txt)|*.txt";
             saveFileDialog.DefaultExt = "txt";
             saveFileDialog.AddExtension = true;
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
@@ -34,18 +80,6 @@ namespace WindowsFormsApp2
                 StreamWriter sw = new StreamWriter(saveFileDialog.FileName);
                 sw.Write(textBox1.Text);
                 sw.Close();
-            }
-        }
-
-        private void LoadButton_Click(object sender, EventArgs e)
-        {
-            OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "All files(*.*)|*.*| Text files (*.txt)|*.txt";
-            if (openFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                StreamReader sr = new StreamReader(openFileDialog.FileName);
-                textBox1.Text = sr.ReadToEnd();
-                sr.Close();
             }
         }
 
@@ -57,23 +91,23 @@ namespace WindowsFormsApp2
             toolStripStatusLabel1.Text = String.Empty;
             if (wordsCountToolStripMenuItem.Checked)
             {
-                toolStripStatusLabel1.Text += $"Words: {wordCount}; ";
+                toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_words")}: {wordCount}; ";
             }
             if (symbolsCountToolStripMenuItem.Checked)
             {
-                toolStripStatusLabel1.Text += $"Symbols: {symbolCount}; ";
+                toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_symbols")}: {symbolCount}; ";
             }
         }
 
-        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
         {
             textBox1.Text = String.Empty;
         }
 
-        private void openToolStripMenuItem1_Click(object sender, EventArgs e)
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
-            openFileDialog.Filter = "All files(*.*)|*.*| Text files (*.txt)|*.txt";
+            openFileDialog.Filter = $"{GetLocal(localGlobal + "_allFiles")}(*.*)|*.*| {GetLocal(localGlobal + "_textFiles")} (*.txt)|*.txt";
             if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 StreamReader sr = new StreamReader(openFileDialog.FileName);
@@ -85,7 +119,7 @@ namespace WindowsFormsApp2
         private void saveToolStripMenuItem_Click(object sender, EventArgs e)
         {
             SaveFileDialog saveFileDialog = new SaveFileDialog();
-            saveFileDialog.Filter = "Text files (*.txt)|*.txt";
+            saveFileDialog.Filter = $"{GetLocal(localGlobal + "_textFiles")} (*.txt)|*.txt";
             saveFileDialog.DefaultExt = "txt";
             saveFileDialog.AddExtension = true;
             if (saveFileDialog.ShowDialog() == DialogResult.OK)
@@ -107,7 +141,7 @@ namespace WindowsFormsApp2
                 symbolsCountToolStripMenuItem.Checked = false;
                 if (wordsCountToolStripMenuItem.Checked)
                 {
-                    toolStripStatusLabel1.Text += $"Words: {wordCount}; ";
+                    toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_words")}: {wordCount}; ";
                 }
             }
             else
@@ -115,9 +149,9 @@ namespace WindowsFormsApp2
                 symbolsCountToolStripMenuItem.Checked = true;
                 if (wordsCountToolStripMenuItem.Checked)
                 {
-                    toolStripStatusLabel1.Text += $"Words: {wordCount}; ";
+                    toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_words")}: {wordCount}; ";
                 }
-                toolStripStatusLabel1.Text += $"Symbols: {symbolCount}; ";
+                toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_symbols")}: {symbolCount}; ";
             }
         }
 
@@ -132,16 +166,16 @@ namespace WindowsFormsApp2
                 wordsCountToolStripMenuItem.Checked = false;
                 if (symbolsCountToolStripMenuItem.Checked)
                 {
-                    toolStripStatusLabel1.Text += $"Symbols: {symbolCount}; ";
+                    toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_symbols")}: {symbolCount}; ";
                 }
             }
             else
             {
-                toolStripStatusLabel1.Text += $"Words: {wordCount}; ";
+                toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_words")}: {wordCount}; ";
                 wordsCountToolStripMenuItem.Checked = true;
                 if (symbolsCountToolStripMenuItem.Checked)
                 {
-                    toolStripStatusLabel1.Text += $"Symbols: {symbolCount}; ";
+                    toolStripStatusLabel1.Text += $"{GetLocal(localGlobal + "_symbols")}: {symbolCount}; ";
                 }
             }
         }
@@ -149,6 +183,30 @@ namespace WindowsFormsApp2
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
+
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = $"{GetLocal(localGlobal + "_allFiles")}: (*.*)|*.*| {GetLocal(localGlobal + "_textFiles")} (*.txt)|*.txt";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                StreamReader sr = new StreamReader(openFileDialog.FileName);
+                textBox1.Text = sr.ReadToEnd();
+                sr.Close();
+            }
+        }
+
+        private void buttonLocalRU_Click(object sender, EventArgs e)
+        {
+            SetLocalization("ru");
+            localGlobal = "ru";
+        }
+
+        private void buttonLocalEN_Click(object sender, EventArgs e)
+        {
+            SetLocalization("en");
+            localGlobal = "en";
         }
     }
 }
